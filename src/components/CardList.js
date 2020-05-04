5a34ba7e2237f20555957cf8a320761b41a55f17
Manny Castaneda <Ecast221@gmail.com>
2020-05-03 22:09:18 -0400

    First

---
+++ src/components/CardList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Card from './Card';
+
+
+const CardList = ({ robots }) => {
+   
+    return (
+        <div>
+            {robots.map((user, i) => {
+                 return <Card key={i} name={user.name} id={user.id} email={user.email}/>
+            })}
+        </div>
+    );
+}
+
+export default CardList;

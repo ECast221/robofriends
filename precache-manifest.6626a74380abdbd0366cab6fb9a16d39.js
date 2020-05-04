28592a78ede78ead4ee66310015e7afc2da17d1b
Manny Castaneda <Ecast221@gmail.com>
2020-05-03 22:12:21 -0400

    Updates

---
+++ precache-manifest.6626a74380abdbd0366cab6fb9a16d39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78cf8981f0aa28f31e6e691c31b259db",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "6ce719015bf6189da3f1",
+    "url": "/robofriends/static/css/2.96716038.chunk.css"
+  },
+  {
+    "revision": "4c83279aef2b3e0e8283",
+    "url": "/robofriends/static/css/main.f5494771.chunk.css"
+  },
+  {
+    "revision": "6ce719015bf6189da3f1",
+    "url": "/robofriends/static/js/2.7d1e7194.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.7d1e7194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c83279aef2b3e0e8283",
+    "url": "/robofriends/static/js/main.f260e1b7.chunk.js"
+  },
+  {
+    "revision": "4b54cd1813776aa306f7",
+    "url": "/robofriends/static/js/runtime-main.de469359.js"
+  }
+]);
